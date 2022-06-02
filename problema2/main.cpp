858c8e2352b0f3534e5b3eb77875457e9375c04b
Juan Cucurell <zarcophage@localhost.localdomain>
2022-06-02 13:31:09 -0400

    ABB implementado según pauta (testeado)

---
+++ problema2/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"ranked-abb.cpp"
+
+int main(){
+    ABB arbol; 
+    if (arbol.raiz == NULL){
+        std::cout<<"raiz creada"<<std::endl;
+    }
+    bool flag = true; int item;
+    while(flag){
+        std::cin>>item;
+        if(item == 1){
+            flag = false;
+        }
+        arbol.insert(item);
+    }
+
+    arbol.inOrder(arbol.raiz);
+
+    std::cout<<"elementos menores o iguales a 6: "<<std::endl;
+    std::cout<<arbol.rank(6)<<std::endl;
+}
--- problema2/ranked-abb.cpp
+++ problema2/ranked-abb.cpp
@@ -3,90 +3,94 @@
 typedef int tElem;
 
 struct nodoABB{
-    elem info;
+    tElem info;
     nodoABB* izq;
     nodoABB* der;
-}
-
-class arbolBin{
-    nodoABB* raiz;
-    int numElem;
-
-    arbolBin(){raiz=NULL;numElem = O};
-    ~arbolBin(){};
-    void clear();
-    void insert(tElem item);
-    void remove(tElem item);
-    bool find(tElem item);
-    int rank(tElem item); //retorna cantidad de elementos menores o iguales a un item, aunque este item esté o no almacenado en el ABB
-    int size();
 };
 
-int rank(tElem item){
-    int counter = 0;
-    return ranker(&counter, raiz, item);
+nodoABB* nodeCreator(tElem item){
+    nodoABB* nuevoNodo = new(nodoABB);
+    nuevoNodo->info = item;
+    nuevoNodo->izq = NULL;
+    nuevoNodo->der = NULL;
+    return nuevoNodo;
 }
 
-void ranker(int &contador, nodoABB* nodo, tElem item){
+class ABB{
+    int numElem;
+    
+    public:
+    nodoABB* raiz;
+    ABB(){numElem = 0; raiz = NULL;};
+    ~ABB(){};
+    //nodoABB* getRaiz(){return raiz;};
+    void insert(tElem x);
+    void inOrder(nodoABB* raiz);
+    bool find(tElem x);
+    int rank(tElem x);
+};
+
+void ABB::inOrder(nodoABB* raiz){
+    if (raiz== NULL){return;}
+    inOrder(raiz->izq);
+    std::cout<<raiz->info<<std::endl;
+    inOrder(raiz->der);
+}
+
+void inOrderRanker(nodoABB* raiz, tElem item, int &elemMenores){
+    if (raiz == NULL){return;}
+    inOrderRanker(raiz->izq, item, elemMenores);
+    if (raiz->info <= item){elemMenores++;}
+    inOrderRanker(raiz->der, item, elemMenores);
+}
+
+int ABB::rank(tElem x){
+    int elemMenores = 0;
+    inOrderRanker(raiz,x,elemMenores);
+    return elemMenores;
+}
+
+void inserter(nodoABB* &nodo, tElem x){
+    if (nodo == NULL){
+        nodo = nodeCreator(x); 
+        std::cout<<"insertado"<<std::endl;
+        return;
+    }
+    if(nodo->info == x){
+        std::cout<<"no se inserta"<<std::endl;
+        return;
+    }
+    if(x > nodo->info){
+        std::cout<<"mayor, movimiento a nodo derecho"<<std::endl;
+        inserter(nodo->der, x);
+    }else if(x < nodo->info){
+        std::cout<<"menor, movimiento a nodo izquierdo"<<std::endl;
+        inserter(nodo->izq, x);
+    }
+}
+
+void ABB::insert(tElem x){
+    inserter(raiz, x);
+}
+
+void finder(nodoABB* nodo, tElem x, bool &flag){
     if (nodo == NULL){
         return;
     }
-    if ((nodo->info)<=item){
-        contador++;
-        ranker(contador, nodo->izq, item);
-    }
-}
-
-bool arbolBin::find(tElem item){
-    return finder(raiz, item)    
-}
-
-bool finder(nodoABB* nodo, tElem item){
-    if (nodo == NULL){
-        return false;
-    };
-    if ((nodo->info)==item){
-        return true;
-    }
-    if (item > (nodo->info)){
-        return finder((nodo-> der),item);
-    }else{
-        return finder((nodo-> izq), item);
-    }
-}
-
-void arbolBin::insert(tElem item){
-    nodoABB nuevoNodo;
-    nuevoNodo.info = item;
-    nuevoNodo.izq = nuevoNodo.der = NULL;
-    temp = findInsertionPlace(item,raiz,&nuevoNodo)
-    numElem++;
-}
-
-void findInsertionPlace(tElem item, nodoABB* nodo, nodoABB &nuevoNodo){
-    if (nodo == NULL){
-        nuevoNodo = nodo;
+    else if (nodo->info == x){
+        flag = true;
         return;
-    if (item > (nodo->info)){
-        findInsertionPlace(item, nodo->der, nuevoNodo);
     }
-    else if (item < (nodo->info)){
-        findInsertionPlace(item,nodo->izq,nuevoNodo);
+    else if (x > nodo->info){
+        finder(nodo->der,x,flag);
+    }else if(x < nodo->info){
+        finder(nodo->izq,x,flag);
     }
 }
 
-void arbolBin::clear(){
-    leafDeleter(raiz);
-    raiz = NULL;
-    numElem = O;
-}
-
-void leafDeleter(nodoABB* nodo){
-    if (nodo == NULL){
-        return;
-    };
-    leafDeleter(nodo->izq);
-    leafDeleter(nodo->der);
-    delete nodo;
+bool ABB::find(tElem x){
+    bool flag = false;
+    finder(raiz, x, flag);
+    return flag;
 }
 
